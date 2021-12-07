c98426d127f9e4acaf8635237cf99e3a402ec1da
“awssaravanan” <saravananc2311@gmail.com>
2021-12-07 02:58:26 +0000

    test javaprogram commit 3 net program

--- test.java
+++ test.java
@@ -4,4 +4,15 @@ System.out.println("saravanan");
 System.out.println("saravanan 2");
 System.out.println("saravanan 3");
 
+System.out.println("saravanan 4");
+<form name="patchinfo" action="/patchdep.do?actionToCall=deployPatch&amp;configName=PATCH_INSTALL&amp;configType=Computer&amp;selectedTab=PatchMgmt" method="post">
+    <input type="hidden" name="resourceID" value="">
+    <input type="hidden" name="isApproveMissingPatch" value="">
+    <input type="hidden" name="isDeployPatch" value="">
+    <input type="hidden" name="shownView" value="">
+    <input type="hidden" name="selectedFilterID" value="">
+    <input type="hidden" name="jsonObj" value="">
+    <input type="hidden" name="patchStr" value="">
+    <input type="hidden" name="autoPopulate" value="">
+</form>
 }}
